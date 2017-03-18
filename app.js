ea465f205189b5d2ea734a09bb22be63310535b3
aek <adil.elkanabi@arrow.com>
2017-03-18 14:51:14 +0100

    git ignore

--- app.js
+++ app.js
@@ -43,5 +43,17 @@ try {
     process.exit(1);
 }
 
+var RtmClient = require('@slack/client').RtmClient;
+var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+var bot_token = process.env.SLACK_BOT_TOKEN || '';
+
+var rtm = new RtmClient(bot_token);
+
+rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
+  console.log('Message:', message); //this is no doubt the lamest possible message handler, but you get the idea
+});
+
+rtm.start();
+
 process.stdout.write(colors.yellow('Listening on port ' + config.port + '...\n'));
 server.listen(config.port);
