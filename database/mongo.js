710bbd5a24a1f51d0498813b90610dd31a1dd0cf
Olivier Lépine <olivier@lepine.fr>
2017-03-18 17:16:04 +0100

    mongo module with scheduled func

--- database/mongo.js
+++ database/mongo.js
@@ -1,29 +1,43 @@
 module.exports = function(config) {
 
-  var MongoClient = require("mongodb").MongoClient;
+    var MongoClient = require("mongodb").MongoClient;
 
-  this.uri = "mongodb://" + config.database.host + "/" + config.database.name
+    this.uri = "mongodb://" + config.database.host + "/" + config.database.name
 
-  this.collection = config.database.collection
+    this.connect = function(callback) {
+        MongoClient.connect(this.uri, function(error, db) {
+            return callback(error, db);
+        });
+    }
 
-  this.connect = function(callback) {
-    MongoClient.connect(this.uri, function(error, db) {
-      console.log(error,db)
-
-        return callback(error, db);
-      });
-
-      console.log("Connecté à la base de données '" + config.database.name + "'");
-  }
-
+    /*
+        Schedule an url retrieving
+    */
     this.schedule = function(data) {
-        this.connect(function(err,db) {
-          console.log(db)
-          db.collection('scheduled').insert(data, null, function (error, results) {
-            if (error) throw error;
 
-            console.log("Le document a bien été inséré")
-          });
-      });
+        if (! data.url) {
+            throw "No URL in object"
+        }
+
+        if (! data.user) {
+            throw "No User in object"
+        }
+
+        if (! data.ts) {
+            throw "No timestamp in object"
+        }
+
+        data.ts = new Date(data.ts*1000)
+        data.created = new Date()
+        console.log(data)
+
+        this.connect(function(err,db) {
+            if (err) throw err
+            db.collection('scheduled').insert(data, null, function (error, results) {
+                if (error) throw error;
+
+                console.log("URL scheduled for parsing")
+            });
+        });
     }
 }
