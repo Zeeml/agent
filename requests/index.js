cb11a22eeca0929ef490da4bd10c19dd80f041d6
aek <adil.elkanabi@arrow.com>
2017-03-18 14:37:01 +0100

    Add body parser

--- requests/index.js
+++ requests/index.js
@@ -1,19 +1,18 @@
+
 module.exports = function(app) {
     var bodyParser = require('body-parser')
     app.use(bodyParser.json())
 
   app.get('/', function(req, res,next) {
-      res.redirect('https://slack.com/oauth/authorize?client_id=117230330418.157030337063&scope=channels:history');
+        var WebClient = require('@slack/client').WebClient;
+        var token = process.env.SLACK_API_TOKEN || ''; //see section above on sensitive data
+        var web = new WebClient(token);
+        web.chat.postMessage('C1232456', 'Hello there', function(err, res) {
+          if (err) {
+              console.log('Error:', err);
+          } else {
+              console.log('Message sent: ', res);
+          }
+        });
   });
-
-  app.get('/success', function(req, res,next) {
-      console.log(req);
-  });
-
-  app.post('/slack/receive', function(req, res,next) {
-      console.log(req.body);
-      res.header("Content-Type",'application/x-www-form-urlencoded');
-      res.send(req.body.challenge);
-  });
-
 }
